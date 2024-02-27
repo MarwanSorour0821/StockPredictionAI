4480b98fd5e1442a8c3b8babcfc32bc70ccab50a
MarwanSorour0821 <lw22182@bristol.ac.uk>
2024-02-27 12:31:23 +0000

    MODIFIED application

--- application.py
+++ application.py
@@ -7,6 +7,7 @@ from pandas_datareader.data import DataReader
 from pandas_datareader import data
 from datetime import datetime
 
+#--------------------------------------------------------- GETTING COMPANY DATA -----------------------------------------------------------------#
 #sns that set aesthetic style of plots
 sns.set_style("whitegrid")
 
@@ -40,8 +41,10 @@ df = pd.concat(company_data.values(), keys=tech_list, names=["Stock", "Date"])
 df.reset_index(inplace=True)
 df.tail(10)
 
+#------------------------------------------------------Plotting Closing Price and Volume -----------------------------------------------------------#
 plt.figure(figsize=(15, 10))
 plt.subplots_adjust(top = 1.25, bottom = 1.2)
+
 
 #We use 1 here for the enumerate because subplot takes values only between 1<= and <= 4
 for i, company_stock_symbol in enumerate(tech_list, 1):
@@ -54,17 +57,24 @@ for i, company_stock_symbol in enumerate(tech_list, 1):
     plt.ylabel("Adj Closing Price")
     plt.xlabel(None)
 
+plt.tight_layout()
+plt.show()
+
+print("Proceeding to volume")
+
+for i, company_stock_symbol in enumerate(tech_list, 1):
+    plt.subplot(2,2,i)
     #Get Volume data for current company symbol
+    company_df = company_data[company_stock_symbol]
     company_df['Volume'].plot()
     plt.title(f'Total volume traded each day for {tech_list[i-1]}')
-    plt.ylabel("Volume Traded")
+    plt.ylabel("Volume Traded Per Day")
     plt.xlabel(None)
 
 plt.tight_layout()
 plt.show()
 
-
-
+#------------------------------------------------ GETTING MOVING AVERAGE OF EACH STOCK --------------------------------------------------#
 
 #Valid periods are: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max
 #print(msft.history(period="max"))
